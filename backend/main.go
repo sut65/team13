8fbef7df75c03ea04daabbe81e126bbd4b0216ca
Nattawut Rodthong <nattavutnon@gmail.com>
2022-12-22 16:24:26 +0700

    Add appbat and setup app.tsx - close #105

--- backend/main.go
+++ backend/main.go
@@ -13,6 +13,7 @@ func main() {
 	entity.SetupDatabase()
 
 	r := gin.Default()
+	r.Use(CORSMiddleware())
 
 	// User Routes
 
@@ -25,5 +26,31 @@ func main() {
 	// Run the server
 
 	r.Run()
+
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+
+			c.AbortWithStatus(204)
+
+			return
+
+		}
+
+		c.Next()
+
+	}
 
 }
