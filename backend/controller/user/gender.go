0bf45c7b8f3589a544ba42aa1b9591198dc7afa6
Nattawut Rodthong <nattavutnon@gmail.com>
2022-12-29 15:56:00 +0700

    fixed #18 note close - close #18

--- backend/controller/user/gender.go
+++ backend/controller/user/gender.go
@@ -11,37 +11,26 @@ import (
 // GET /gender/:id
 
 func GetGender(c *gin.Context) {
-
 	var gender entity.Gender
-
 	id := c.Param("id")
 
 	if err := entity.DB().Raw("SELECT * FROM genders WHERE id = ?", id).Scan(&gender).Error; err != nil {
-
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-
 		return
-
 	}
 
 	c.JSON(http.StatusOK, gin.H{"data": gender})
-
 }
 
 // GET /users
 
 func ListGenders(c *gin.Context) {
-
 	var genders []entity.Gender
 
 	if err := entity.DB().Raw("SELECT * FROM genders").Scan(&genders).Error; err != nil {
-
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-
 		return
-
 	}
 
 	c.JSON(http.StatusOK, gin.H{"data": genders})
-
 }
