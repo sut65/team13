0a48ab103c5a716bffff8582594e41de1ac8582b
Nattawut Rodthong <nattavutnon@gmail.com>
2022-12-30 11:02:19 +0700

    add some code in frontend USER

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FullAppBar from "./components/FullAppBar";
 
 import Home from "./components/Home";
+import User_UI from "./components/user/User_UI"
 
 export default function App() {
   // const [token, setToken] = React.useState<String>("");
@@ -24,6 +25,8 @@ export default function App() {
       <FullAppBar />
       <Routes>
         <Route path="/" element={<Home/>} /> {/** home */}
+        {/** <Route path="/" element={<Home/>} /> {/** home */}
+        <Route path="/user_store_setting" element={<User_UI/>} /> {/** user & store setting */}
       </Routes>
     </div>
   </Router>
