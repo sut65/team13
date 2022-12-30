e1fe97a80f503281c17c74e03c8403c2af660ce8
unknown <nathawutrock@gmail.com>
2022-12-30 20:55:20 +0700

    Add Controller game_status - close #113

---
+++ backend/controller/game/game_status.go
@@ -0,0 +1,40 @@
+package controller
+
+import (
+	"github.com/sut65/team13/entity" // เรียกเพื่อเรียกใช้ฟังก์ชั่นใน setup.go (มันจะถูก declare อัตโนมัติว่าตัวมันเองเป็น entity)
+
+	"github.com/gin-gonic/gin"
+
+	"net/http"
+)
+
+// GET /Game_status // id
+
+func GetGame_status(c *gin.Context) {
+	var game_status entity.Game_Status
+	id := c.Param("id")
+
+	if err := entity.DB().Raw("SELECT * FROM game_status WHERE id = ?", id).Scan(&game_status).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": game_status})
+}
+
+// Get // Game_status
+func ListGame_status(c *gin.Context) {
+
+	var game_status []entity.Game_Status
+
+	if err := entity.DB().Raw("SELECT * FROM game_status").Scan(&game_status).Error; err != nil {
+
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+
+		return
+
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": game_status})
+
+}
