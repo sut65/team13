e7cde3a98a05d348a798c0322172f2cb91cf220c
phanukorn2644 <phanukorn2644@gmail.com>
2023-01-09 16:31:28 +0700

    ListStoragesUser - Close #138

--- backend/controller/storage/storage.go
+++ backend/controller/storage/storage.go
@@ -81,7 +81,7 @@ func ListStoragesUser(c *gin.Context) {
 	id := c.Param("id")
 
 	// มี prelaod เพื่อใช้โหลดให้ระบบ Storages
-	if err := entity.DB().Preload("Game").Raw("SELECT * FROM storages WHERE id = ?", id).Find(&storages).Error; err != nil {
+	if err := entity.DB().Preload("Game").Preload("User").Raw("SELECT * FROM storages WHERE user_id = ?", id).Find(&storages).Error; err != nil {
 
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 
