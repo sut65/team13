71d8224c9b0138745d6ea2c98e9c9dc89eb8dcdc
Nattawut Rodthong <nattavutnon@gmail.com>
2023-01-20 11:20:34 +0700

    edit some dash board

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import { Container } from "@material-ui/core";
-import { Box, Grid, Paper } from '@mui/material';
+import { Box, Grid, Paper, TextField } from '@mui/material';
+
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
 
 import { GamesInterface } from '../models/game/IGame';
 
 function Dashboard(){
     const [games, setGames] = React.useState<GamesInterface[]>([]);
+    const [searchQuery, setSearchQuery] = React.useState("");
 
     const getGame = async () => {
         const apiUrl = "http://localhost:8080/Game";
@@ -26,28 +30,70 @@ function Dashboard(){
             });
     };
 
-    function show_game_list(ID: any) {
-        if(ID == -1){ // ID เป็น -1 หมายถึงตอนอ่านค่ามา เป็น null (ไม่มี fav game) แล้วมันถูกแปลงให้เป็น = 0 auto ผ่าน Number() | แล้วพอถูก -1 จากการผ่าน argument เลยกลายเป็น -1
-            return null;
-        }else{
-            return(
-                <Grid>
-                    {games[Number(ID)].Game_Name}
-                </Grid>
-            );
+    const filterData = (query: any, data: any) => {
+        if (!query) {
+          return data;
+        } else {
+          return data.filter((d: any) => d.toLowerCase().includes(query.toLowerCase()));
         }
-    }
+    };
+
+    let game_name_array = games.map((item: GamesInterface) => (item.Game_Name));
+    const dataFiltered = filterData(searchQuery, game_name_array);
 
     React.useEffect(() => {
-
+        getGame();
     }, []);
 
     return (
         <Container>
             <Box>
                 <Paper elevation={2} sx={{padding:2,margin:2}}>
-                    <Grid container justifyContent={"center"}>
+                    <Grid container justifyContent={"center"}> {/** Banner */}
                         <img src={``} width="700" height="300"/>
+                    </Grid>
+                    <Grid> {/** Search */}
+                    <div
+                    style={{
+                        display: "flex",
+                        alignSelf: "center",
+                        justifyContent: "center",
+                        flexDirection: "column",
+                        padding: 20
+                    }}>
+
+                        <TextField
+                            id="search-bar"
+                            onChange={(event) => (
+                                setSearchQuery(event.target.value)
+                            )}
+                            label="Search a game by name"
+                            variant="outlined"
+                            //placeholder="Search..."
+                            size="small"
+                        />
+
+                        <div style={{ padding: 3 }}>
+                            {dataFiltered.map((d: any) => (
+                                <div
+                                    className="text"
+                                    style={{
+                                    padding: 5,
+                                    justifyContent: "normal",
+                                    fontSize: 20,
+                                    color: "blue",
+                                    margin: 1,
+                                    width: "250px",
+                                    //BorderColor: "Greeb",
+                                    borderWidth: "10px"
+                                    }}
+                                    key={d.id}
+                                >
+                                    {d}
+                                </div>
+                                ))}
+                            </div>
+                        </div>
                     </Grid>
                 </Paper>
             </Box>
