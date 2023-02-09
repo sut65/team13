e1a4490f69f5fdf47f8921b55f6e90f1a5a120cb
bthananya <b6307431@g.sut.ac.th>
2023-02-10 02:42:51 +0700

    valid date - close #69

--- backend/entity/schema.go
+++ backend/entity/schema.go
@@ -253,10 +253,10 @@ type Order struct {
 	Verification_Status_ID *uint               `valid:"-"`
 	Verification_Status    Verification_Status `gorm:"references:id" valid:"-"`
 	Slip                   string              `valid:"image_valid~รูปภาพไม่ถูกต้อง,required~กรุณาอัปโหลดรูปภาพ"`
-	Date                   time.Time `valid:"DelayNow10Min~เวลาเป็นอดีต กรุณาโหลดหน้าเว็บใหม่"`
-	Send_gift              *bool  `valid:"-"`
-	Friend_ID              *uint  `valid:"-"`
-	Friend                 Friend `gorm:"references:id" valid:"-"`
+	Date                   time.Time           `valid:"DelayNow10Min~เวลาเป็นอดีต กรุณาโหลดหน้าเว็บใหม่"`
+	Send_gift              *bool               `valid:"-"`
+	Friend_ID              *uint               `valid:"-"`
+	Friend                 Friend              `gorm:"references:id" valid:"-"`
 
 	Basket               []Basket               `gorm:"foreignKey:Order_ID"`
 	Payment_Verification []Payment_Verification `gorm:"foreignKey:Order_ID"`
@@ -280,8 +280,8 @@ type Payment_Verification struct {
 	Order                  Order               `gorm:"references:id" valid:"-"`
 	Verification_Status_ID *uint               `valid:"-"`
 	Verification_Status    Verification_Status `gorm:"references:id" valid:"-"`
-	Date                   time.Time
-	Note                   string `valid:"required~Note cannot be blank"`
+	Date                   time.Time           `valid:"DelayNow10Min~เวลาเป็นอดีต กรุณาโหลดหน้าเว็บใหม่"`
+	Note                   string              `valid:"required~Note cannot be blank"`
 }
 
 // ----ระบบ Wishlist---
